838beb9cf87fb5401c2abe7206f08d0272ee83b8
Tiago <TVTH059@gmail.com>
2023-11-14 11:42:23 +0100

    changed some stuff
    heyheyhey

---
+++ Assets/Scripts/CompositeBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Composite")]
+public class CompositeBehaviour : FlockBehavior
+{
+    public FlockBehavior[] behaviours;
+    public float[] weights;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        if (weights.Length != behaviours.Length) 
+        {
+            return Vector2.zero;
+        }
+
+        Vector2 move = Vector2.zero;
+
+        for (int i = 0; i < behaviours.Length; i++) 
+        {
+            Vector2 partialMove = behaviours[i].CalculateMove(agent, context, flock) * weights[i];
+            
+            if (partialMove != Vector2.zero) 
+            {
+                if (partialMove.sqrMagnitude > weights[i] * weights[i]) 
+                {
+                    partialMove.Normalize();
+                    partialMove *= weights[i];
+                }
+                move += partialMove;
+            }
+        }
+
+        return move;
+    }
+}
---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockAgent agentPrefab;
+    private List<FlockAgent> agents = new List<FlockAgent>();
+    public FlockBehavior behavior;
+
+    [Range(10, 500)]
+    public int startingCount = 250;
+    const float AgentDensity = 0.08f;
+
+    [Range(1f, 100f)]
+    public float driveFactor = 10f;
+
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+
+    [Range(1f, 10f)]
+    public float neighbourRadius = 1.5f;
+
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    private float squareMaxSpeed;
+    private float squareNeighborRadius;
+    private float squareAvoidanceRadius;
+    public float squareAvoidanceRadiusGET { get { return squareAvoidanceRadius; } }
+
+    void Start()
+    {
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighborRadius = neighbourRadius * neighbourRadius;
+        squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+
+        for (int i = 0; i < startingCount; i++) 
+        {
+            FlockAgent newAgent = Instantiate(
+                agentPrefab, 
+                Random.insideUnitCircle * startingCount * AgentDensity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f,360f)),
+                transform
+                );
+            newAgent.name = "Agent #" + i;
+            agents.Add(newAgent);
+        }
+    }
+
+    void Update()
+    {
+        foreach (FlockAgent agent in agents) 
+        {
+            List<Transform> context = GetNearbyObjects(agent);
+            //agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.green, context.Count /6f);
+
+
+            Vector2 move = behavior.CalculateMove(agent, context, this);
+            move *= driveFactor;
+            if (move.sqrMagnitude > squareMaxSpeed)
+            {
+                move = move.normalized * maxSpeed;
+            }
+            agent.Move(move);
+        }
+    }
+
+    List<Transform> GetNearbyObjects(FlockAgent agent) 
+    {
+        List<Transform> context = new List<Transform>();
+        Collider2D[] contextColliders = Physics2D.OverlapCircleAll(agent.transform.position, neighbourRadius);
+        foreach (Collider2D c in contextColliders) 
+        {
+            if (c != agent.agentColliderGET) 
+            {
+                context.Add(c.transform);
+            }
+        
+        }
+        return context;
+    }
+}
